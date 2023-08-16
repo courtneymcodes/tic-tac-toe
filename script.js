f47fd9d250e1c97dd8600aae8fcaec42ae8d7b33
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-08-16 14:44:29 -0400

    get checkWinner function working

--- script.js
+++ script.js
@@ -28,9 +28,31 @@ allSquares.forEach((square, index) => {
           currentPlayerTurn === 1 ? currentPlayerTurn = 2 : currentPlayerTurn = 1
           //push the id of selected square to current player's positions array
           currentPlayerTurn === 1 ? player1.positions.push(square.id) : player2.positions.push(square.id)
-          
+          //if player1's turn, check if player1 has won. If not then its player2's turn, check if player2 has won
+          currentPlayerTurn === 1 ? checkWinner(player1) : checkWinner(player2)
         }
     }) 
 })
 
+//indexes player needs in player.positions to win
+const winningCombinations = ['012', '345', '678', '048', '246']
+
+//variable to track if a player has won 
+let playerWon; 
+
+
+function checkWinner(player) {
+  let currentPlayerPositions = player.positions  //get array of current player's positions
+  currentPlayerPositions = currentPlayerPositions.join('') //convert array to a string
+  //check if any of the winning conditions are in the player positions string:
+  //iterate of the winning combinations array. For each winning combination, check if the player's position str contains all 3 numbers in the current combination
+  winningCombinations.forEach((combination) => {
+    let check0 = currentPlayerPositions.search(combination[0])  //the search method returns -1 if not found or the position if is found
+    let check1 = currentPlayerPositions.search(combination[1])
+    let check2 = currentPlayerPositions.search(combination[2])
+    //if all numbers in the current combination exist in the playerPositions array, set winner to true. If not, set winner to false
+    check0 >= 0 && check1 >= 0 && check2 >= 0 ? playerWon = true : playerWon = false
+    console.log(`playerWon: ${playerWon}`)
+})  
+}
 
