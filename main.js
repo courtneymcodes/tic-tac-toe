62624d3a6b0405e80dcd51c2e825589bfbef430f
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-08-18 18:09:37 -0400

    set up second game board

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const chooseGameContainer = document.querySelector('#choose-game')
 const colorsGameContainer = document.querySelector('#colors-game')
-//const underwaterGameContainer = document.querySelector('#underwater-game')
+const underwaterGameContainer = document.querySelector('#underwater-game-container')
 const colorsGameButton = document.querySelector('#colors-button')
 const underwaterGameButton = document.querySelector('#underwater-button')
 
-colorsGameContainer.style.display = 'none'  //hide all of the game elements
+//hide all of the game elements
+colorsGameContainer.style.display = 'none'
+underwaterGameContainer.style.display = 'none'
 
-//add event listener to both buttons (do seperately)
-//when clicked: 
-//set style.display = 'none' of choose-game div to hide it
-//unhide the div of the colors game or underwater game - game should start working
-
+//add event listener to both buttons
 colorsGameButton.addEventListener('click', () => {
   chooseGameContainer.style.display = 'none'  //hide game choices elements currently displayed
-  colorsGameContainer.style.display = ""  //unhide colors game elements
+  colorsGameContainer.style.display = ''  //unhide colors game elements to start game
 })
-
+underwaterGameButton.addEventListener('click', () => {
+    chooseGameContainer.style.display = 'none'
+    underwaterGameContainer.style.display = ''
+})
